cab8d1e52282f54689bec059475bba54005711d5
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-24 22:04:04 +0200

    Update: AUTOSAR basic software service state management module for CAN communication protocol stack

--- main_LATEST/ServiceCanSm.cpp
+++ main_LATEST/ServiceCanSm.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* File   : ServiceCanSm.cpp                                                         */
+/* File   : ServiceCanSm.cpp                                                  */
 /* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
 /******************************************************************************/
 
@@ -13,8 +13,8 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define SERVICECANSM_AR_RELEASE_VERSION_MAJOR                                         4
-#define SERVICECANSM_AR_RELEASE_VERSION_MINOR                                         3
+#define SERVICECANSM_AR_RELEASE_VERSION_MAJOR                                  4
+#define SERVICECANSM_AR_RELEASE_VERSION_MINOR                                  3
 
 /******************************************************************************/
 /* MACROS                                                                     */
