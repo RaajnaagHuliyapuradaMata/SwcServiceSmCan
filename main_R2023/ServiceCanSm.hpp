1147725a2769f1e3ba404eefc2bcdba04475c847
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-12-27 20:39:13 +0100

    Update: AUTOSAR basic software service state management module for CAN communication protocol stack

---
+++ main_R2023/ServiceCanSm.hpp
@@ -0,0 +1,67 @@
+#pragma once
+/******************************************************************************/
+/* File   : ServiceCanSm.hpp                                                         */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "ConstServiceCanSm.hpp"
+#include "CfgServiceCanSm.hpp"
+#include "ServiceCanSm_core.hpp"
+#include "infServiceCanSm_Exp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class module_ServiceCanSm:
+      INTERFACES_EXPORTED_SERVICECANSM
+      public abstract_module
+   ,  public class_ServiceCanSm_Functionality
+{
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+      const ConstServiceCanSm_Type* lptrConst = (ConstServiceCanSm_Type*)NULL_PTR;
+      infServicePduRClient_Up infServicePduRClient_ServiceCanSm;
+
+   public:
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+      FUNC(void, SERVICECANSM_CODE) InitFunction(
+            CONSTP2CONST(ConstModule_TypeAbstract, SERVICECANSM_CONST,       SERVICECANSM_APPL_CONST) lptrConstModule
+         ,  CONSTP2CONST(CfgModule_TypeAbstract,   SERVICECANSM_CONFIG_DATA, SERVICECANSM_APPL_CONST) lptrCfgModule
+      );
+      FUNC(void, SERVICECANSM_CODE) DeInitFunction (void);
+      FUNC(void, SERVICECANSM_CODE) MainFunction   (void);
+      SERVICECANSM_CORE_FUNCTIONALITIES
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+extern VAR(module_ServiceCanSm, SERVICECANSM_VAR) ServiceCanSm;
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
