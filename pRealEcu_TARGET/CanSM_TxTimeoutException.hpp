dd55eef2995723d79d82a5f15ecdaad0f115192d
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-10-16 08:11:05 +0200

    AUTOSAR - Hotfixes - pRealEcu_cR_TARGET - SwcServiceCanSm

---
+++ pRealEcu_TARGET/CanSM_TxTimeoutException.hpp
@@ -0,0 +1,9 @@
+#ifndef CANSM_TXTIMEOUTEXCEPTION_H
+#define CANSM_TXTIMEOUTEXCEPTION_H
+
+#include "CanSM_Cfg.hpp"
+#include "ComM.hpp"
+
+extern FUNC(void, CANSM_CODE) CanSM_TxTimeoutException(VAR(NetworkHandleType, AUTOMATIC) Channel);
+
+#endif
---
+++ pRealEcu_TARGET/b_git_push.sh
@@ -0,0 +1,4 @@
+git add .
+git status
+git commit -m "AUTOSAR - Hotfixes - pRealEcu_cR_TARGET - SwcServiceCanSm"
+git push
