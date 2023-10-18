ba85cd2f986ba56b1b26686e7bcb27f92a6936ff
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-10-18 14:23:07 +0200

    AUTOSAR - code freeze - pRealEcu_cR_R2023.10.18 - SwcServiceCanSm

---
+++ pRealEcu_R2023.10.18/CanSM_Cbk.hpp
@@ -0,0 +1,14 @@
+#ifndef CANSM_CBK_H
+#define CANSM_CBK_H
+
+#include "CanSM_Cfg.hpp"
+#include "CanIf.hpp"
+
+extern FUNC(void, CANSM_CODE) CanSM_ControllerBusOff(VAR(uint8, AUTOMATIC) ControllerId);
+extern FUNC(void, CANSM_CODE) CanSM_ControllerModeIndication(
+      VAR(uint8, AUTOMATIC) ControllerId
+   ,  VAR(Type_EcuabCanIf_eModesController, AUTOMATIC) ControllerMode
+);
+
+#endif
+
